4c9766916aa2a0e0a2f858970cfea7cc95bee2a7
Praveen <praveen.suryanarayanamoorthy@cesltd.com>
2019-04-15 22:02:30 +0530

    client

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
+import './App.css';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+if (localStorage.jwtToken) {
+  debugger
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser(this.props.history));
+    // Redirect to login
+    window.location.href = "/login";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+     <Router>
+      <div className="App">
+        <Navbar/>
+        <Route exact path="/" component = {Landing}/>
+        <Route exact path="/" component = {Register}/>
+        <Route exact path="/" component = {Login}/>
+        <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+      </div>
+      </Router>
+      </Provider >
+    );
+  }
+}
+
+export default App;
